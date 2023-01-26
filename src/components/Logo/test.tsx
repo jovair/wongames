fe8d95552f5522643f3158603531ab8df7057e1c
Jovair <jvr_ribeiro@live.com>
2023-01-26 17:51:50 -0300

    Aulas 148-150 - Logo finalizada

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react'
+import 'jest-styled-components'
 
 import Logo from '.'
 import { renderWithTheme } from 'utils/tests/helpers'
@@ -9,5 +10,33 @@ describe('<Logo />', () => {
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: '#030517'
     })
+  })
+  it('should render a white vabel when color is passed', () => {
+    renderWithTheme(<Logo color="white" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+  it('should render a normal size by default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+  it('should render a large size when size is passed', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
+  it('should render a bigger logo without text if hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    )
   })
 })
